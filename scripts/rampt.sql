04593eda492f20e2804420161a936d7e096ef1f8
petreli12 <87396559+petreli12@users.noreply.github.com>
2023-11-14 18:41:46 -0700

    Update rampt.sql

--- scripts/rampt.sql
+++ scripts/rampt.sql
@@ -1,9 +1,19 @@
 SELECT
-  DATE_TRUNC('day', transaction_time) AS transaction_date,
-  AVG(transaction_amount) OVER (ORDER BY DATE_TRUNC('day', transaction_time) RANGE BETWEEN INTERVAL '2' DAY PRECEDING AND CURRENT ROW) AS rolling_avg_3_day
-FROM
-  transactions
+  transaction_date,
+  AVG(transaction_amount) AS rolling_avg_3_day
+FROM (
+  SELECT
+    DATE(transaction_time) AS transaction_date,
+    transaction_amount
+  FROM
+    transactions
+) AS date_amount
 WHERE
-  DATE_TRUNC('day', transaction_time) BETWEEN '2021-01-29' AND '2021-01-31'
+  transaction_date BETWEEN '2021-01-29' AND '2021-01-31'
+GROUP BY
+  transaction_date
+HAVING
+  transaction_date = '2021-01-31'
 ORDER BY
   transaction_date;
+
